09ed9c123ca2738c8df703f57590d7d2cc8102e7
schmidtGabriel <g.avilasouza@gmail.com>
2022-03-16 15:46:03 -0300

    Update me.tsx
    fix me

--- src/pages/api/auth/me.tsx
+++ src/pages/api/auth/me.tsx
@@ -1,62 +1,39 @@
-import jwt  from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import User from 'models/User';
-import {NextApiRequest, NextApiResponse} from 'next';
-import dbConnect from 'utils/mongodb';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "models/User";
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "utils/mongodb";
+import middlewareTokenVerify from "../middlewares/tokenVerify";
 const message = require("../../../utils/messages");
 
 dbConnect();
 
-async function generateToken(params){
-    const token =  jwt.sign({params}, process.env.SECRET_KEY)
-    return token
- }
+async function generateToken(params) {
+  const token = jwt.sign({ params }, process.env.SECRET_KEY);
+  return token;
+}
 
- async function middlewareTokenVerify(authorization, req, res){
+export default async function (req, res) {
+  const { authorization } = req.headers;
 
-    if(!authorization)
-    return res.status(401).json({success: false, msg: 'No token provider'})
+  try {
+    await middlewareTokenVerify(authorization, req, res);
 
-    const parts = authorization.split(' ');
-    if(parts.length !== 2)
-    return res.status(401).json({success: false, msg: 'Token Error'})
+    if (req.userId) {
+      const user = await User.findById(req.userId);
 
-    if(!/^DoNada$/i.test(parts[0]))
-    return res.status(401).json({sucess: false, msg: 'Token bad formatted'})
+      if (!user) {
+        res.status(400).send(message(2));
+      }
 
-    jwt.verify(parts[1], process.env.SECRET_KEY , (err, decoded) => {
-        if(err) return res.status(401).json({success: false, msg: 'Token invalid'})
-        
-        req.userId = decoded.params.id;
-        return true
-    })
-    
- }
+      user.password = undefined;
+      user.confirmPassword = undefined;
 
-export default async function(req, res){
+      const token = await generateToken({ id: user._id });
 
-
-    const { authorization } = req.headers;
-
-    try{
-
-        await middlewareTokenVerify(authorization, req, res)
-        
-        const user = await User.findById(req.userId)
-
-        if(!user){
-            res.status(400).send(message(2))
-        }
-
-        user.password = undefined
-        user.confirmPassword = undefined
-
-        const token = await generateToken({id: user._id})
-
-        res.status(200).send(message(0, {user: user, token: token}))
-    
-    }catch(error){
-        res.status(400).send(message(1))
+      res.status(200).send(message(0, { user: user, token: token }));
     }
-    
+  } catch (error) {
+    res.status(400).send(message(1));
+  }
 }
