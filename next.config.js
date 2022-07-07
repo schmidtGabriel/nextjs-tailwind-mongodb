999d309981e7821eafb787f8f7c9252d6e407458
schmidtGabriel <g.avilasouza@gmail.com>
2022-07-07 18:35:09 -0300

    updating project

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    env:{
-      MONGODB_URI: "mongodb://localhost/easymenu",
-      APP_URL: "http://localhost:3000/",
-      SECRET_KEY: "k31jpajdp32mmadDoNadaLSLKS98ADN"
-    },
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/admin',
-          permanent: false,
-        }
-      ]
-    },
-  }
-  //      SECRET_KEY: "k31jpajdp32mmadDoNadaLSLKS98ADN"
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  env: {
+    MONGODB_URI: "mongodb://localhost/easymenu",
+    APP_URL: "http://localhost:3000/",
+    SECRET_KEY: "k31jpajdp32mmadDoNadaLSLKS98ADN",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+      },
+    ];
+  },
+};
+//      SECRET_KEY: "k31jpajdp32mmadDoNadaLSLKS98ADN"
